34865a1e610f410c005262c16b7d67768f1642e9
Ilyas Farras <73259186+Ilyasfarras12@users.noreply.github.com>
2023-11-01 13:09:38 +0700

    Add files via upload

---
+++  Kendaraan.py
@@ -0,0 +1,18 @@
+# tugas pekan 6
+#buat variable dengan value
+
+profil = ["motor","sepeda motor","300","putih","dua"]
+
+print(profil)
+
+profil.append("60.000.000")
+profil.append("matic")
+
+print(profil)
+
+profil.insert(2,"vespa matic")
+
+
+print(profil)
+
+ 
